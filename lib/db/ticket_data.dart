d49a4493566790e6b23d08f164d474e3eb7ea648
AndrewVorotyntsev <andrey12092000@gmail.com>
2023-04-09 19:29:13 +0300

    дока

--- lib/db/ticket_data.dart
+++ lib/db/ticket_data.dart
@@ -1,16 +1,23 @@
-import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 
 part 'ticket_data.g.dart';
 
+/// Класс билета для хранения в базе данных
 @HiveType(typeId: 0)
 class TicketData {
+  /// Название билета
   @HiveField(0)
   String name;
+
+  /// Ссылка на билет
   @HiveField(1)
   String url;
+
+  /// Ссылка на локальный файл
   @HiveField(2)
   String? source;
+
+  /// Дата добавления билета
   @HiveField(3)
   DateTime created;
 
