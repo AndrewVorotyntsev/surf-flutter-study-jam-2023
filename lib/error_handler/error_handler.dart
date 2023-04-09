a733fb896dbca5fd709748306d95d420535e438d
AndrewVorotyntsev <andrey12092000@gmail.com>
2023-04-09 19:31:36 +0300

    дока

--- lib/error_handler/error_handler.dart
+++ lib/error_handler/error_handler.dart
@@ -1,10 +1,13 @@
 import 'package:elementary/elementary.dart';
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 
+/// Обработчик ошибок приложения
 @singleton
 class AppErrorHandler extends ErrorHandler {
+  /// Обработать ошибку
   @override
   void handleError(Object error, {StackTrace? stackTrace}) {
-    print(error);
+    debugPrint(error.toString());
   }
 }
