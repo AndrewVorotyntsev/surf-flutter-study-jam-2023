bec5c9c12adf58d7939cf14c0b5dd2e666368e23
AndrewVorotyntsev <andrey12092000@gmail.com>
2023-04-09 11:27:47 +0300

    Виджет для билета

---
+++ lib/uikit/ticket_card_widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:surf_flutter_study_jam_2023/res/colors.dart';
+
+class TicketCardWidget extends StatelessWidget {
+  /// Название билета
+  final String name;
+
+  /// Прогресс скачивания файла
+  final double progress;
+
+  /// Статус скачивания файла
+  final String status;
+
+  /// Скачанный объем
+  final double? currentLoaded;
+
+  /// Общий размер файла
+  final double? total;
+
+  const TicketCardWidget({
+    super.key,
+    required this.name,
+    required this.progress,
+    required this.status,
+    this.currentLoaded,
+    this.total,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: cardShadowColor.withOpacity(0.3),
+            spreadRadius: 0,
+            blurRadius: 20,
+            offset: const Offset(0, 0),
+          ),
+        ],
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 16,
+          ),
+          Icon(Icons.airplane_ticket),
+          SizedBox(
+            width: 30,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  textScaleFactor: 1.4,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                SizedBox(
+                  width: 200,
+                  child: LinearProgressIndicator(
+                    minHeight: 5,
+                    value: progress,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(status)
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Icon(Icons.download),
+          SizedBox(
+            width: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
